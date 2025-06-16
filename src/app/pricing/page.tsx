007e039ba4d32d028a1b4075a0049886636cd546
mubashirbappi <mubashirbappi@gmail.com>
2025-06-16 16:16:13 +0600

    make a basic plan card in pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,25 +1,74 @@
-import ToggleButton from '@/components/Pricing/ToggleButton';
-import React from 'react';
-
+import ToggleButton from "@/components/Pricing/ToggleButton";
+import React from "react";
+import { CircleCheck } from "lucide-react";
 const Pricing = () => {
   return (
     <div>
-
-        {/* Pricing hero section */}
+      {/* Pricing hero section */}
       <div className="md:container mx-auto md:mb-40 mb-7 md:p-8 p-2 text-[#0A1532] text-center">
         <h1 className="md:text-6xl text-4xl mt-20 text-center font-semibold leading-relaxed md:leading-[5rem]">
-          Simple, Transparent Pricing To <br /> Unlock Global Real Estate Insights
+          Simple, Transparent Pricing To <br /> Unlock Global Real Estate
+          Insights
         </h1>
         <p className="mt-6 text-xl md:text-xl text-center text-[#80838A] leading-loose">
-          Access global real estate insights tailored to your needs. No hidden fees, cancel anytime.
+          Access global real estate insights tailored to your needs. No hidden
+          fees, cancel anytime.
         </p>
 
-       <div className='flex justify-center mt-10'>
-        <ToggleButton/>
+        <div className="flex justify-center mt-10">
+          <ToggleButton />
+        </div>
+        <section>
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-4  mt-20 mb-10  mx-auto">
+            {/* Basic Plan */}
+            <div className="relative bg-white p-8 rounded-lg  border border-[#0A1532] w-full  mx-auto">
+              <div className="absolute top-4 right-4 bg-[#0A1532] text-white text-xs font-semibold px-3 py-1 rounded-full uppercase tracking-wide shadow-lg">
+                Best Plan
+              </div>
 
+              <div className="flex flex-col items-start mb-6">
+                <h2 className="text-2xl font-bold text-[#0A1532] mb-1">
+                  Basic Tier
+                </h2>
+                <h4 className="text-gray-600">For Individual Analysts</h4>
+              </div>
 
+              <div className="mb-6">
+                <p className="text-3xl font-bold text-[#0A1532] mb-2">
+                  $99/month
+                </p>
+                <p className="text-gray-500 text-sm">
+                  Save 15% annually ($296/year)
+                </p>
+              </div>
 
-       </div>
+              <button className="bg-[#0A1532] text-white w-full py-3 rounded-lg text-center font-medium transition hover:bg-[#0d1d45]">
+                Get Started
+              </button>
+
+              <ul className="text-left space-y-3 mt-6 text-gray-700">
+                <li className="flex items-start">
+                  <span className="mr-2">
+                    <CircleCheck />
+                  </span>
+                  Access to 100+ global markets
+                </li>
+                <li className="flex items-start">
+                  <span className=" mr-2">
+                    <CircleCheck />
+                  </span>
+                  Basic analytics tools
+                </li>
+                <li className="flex items-start">
+                  <span className=" mr-2">
+                    <CircleCheck />
+                  </span>
+                  Email support
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
