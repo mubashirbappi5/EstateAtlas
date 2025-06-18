bdcbbf82347829e167b8a9ae50c04b445f17e096
mubashirbappi <mubashirbappi@gmail.com>
2025-06-18 10:21:41 +0600

    refine home page

--- src/components/Home/WhyChoose.tsx
+++ src/components/Home/WhyChoose.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image';
 import React from 'react';
 import cardimg from '../../../public/card1.png'
 import global from '../../../public/global.png'
+import data from '../../../public/Data.png'
+import Insight from '../../../public/KeyHome.png'
+import Analytics from '../../../public/Analaytic.png'
 const WhyChoose = () => {
     return (
         <div className='py-10 container mx-auto'>
@@ -43,31 +46,31 @@ const WhyChoose = () => {
                     <div className=' space-y-6 p-4 md:border-none border'>
                        <div>
 
-                           <Image src={global} alt='global' width={70} height={70}></Image>
+                           <Image src={data} alt='global' width={70} height={70}></Image>
                        </div>
                        <div className='space-y-4'>
-                        <h1 className='text-2xl font-bold text-[#000929]'>Global Market Coverage</h1>
-                        <p className='text-[#4D5461]'>Access verified data from 100+ global  markets, updated hourly. Compare  trends, yields, and risks across regions  with a single click.</p>
+                        <h1 className='text-2xl font-bold text-[#000929]'>Data-Driven Accuracy</h1>
+                        <p className='text-[#4D5461]'>Rely on rigorously audited data from trusted sources, including governments, agencies, and on-the-ground analysts.</p>
                        </div>
                     </div>
                     <div className=' md:border-none border space-y-6 p-4'>
                        <div>
 
-                           <Image src={global} alt='global' width={70} height={70}></Image>
+                           <Image src={Insight} alt='global' width={70} height={70}></Image>
                        </div>
                        <div className='space-y-4 '>
-                        <h1 className='text-2xl font-bold text-[#000929]'>Global Market Coverage</h1>
-                        <p className='text-[#4D5461]'>Access verified data from 100+ global  markets, updated hourly. Compare  trends, yields, and risks across regions  with a single click.</p>
+                        <h1 className='text-2xl font-bold text-[#000929]'>Transparent Insights</h1>
+                        <p className='text-[#4D5461]'>Track real-time metrics like vacancy rates, rental yields, and price  trends. Export PDF/CSV reports instantly for client presentations.</p>
                        </div>
                     </div>
                     <div className=' space-y-6 p-4 md:border-none border'>
                        <div>
 
-                           <Image src={global} alt='global' width={70} height={70}></Image>
+                           <Image src={Analytics} alt='global' width={70} height={70}></Image>
                        </div>
                        <div className='space-y-4'>
-                        <h1 className='text-2xl font-bold text-[#000929]'>Global Market Coverage</h1>
-                        <p className='text-[#4D5461]'>Access verified data from 100+ global  markets, updated hourly. Compare  trends, yields, and risks across regions  with a single click.</p>
+                        <h1 className='text-2xl font-bold text-[#000929]'>Tailored Analytics</h1>
+                        <p className='text-[#4D5461]'>Build custom dashboards, set alerts for market shifts, and automate workflows. Control every aspect of your analysis.</p>
                        </div>
                     </div>
                     
