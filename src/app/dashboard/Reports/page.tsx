0d6db40957d2adb98fd5e67f48343bcd1bf8364c
mubashirbappi <mubashirbappi@gmail.com>
2025-06-22 16:12:26 +0600

    make report page

--- src/app/dashboard/Reports/page.tsx
+++ src/app/dashboard/Reports/page.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Reports = () => {
     return (
-        <div>
-            <h1>this is reports page</h1>
+        <div className='w-full h-screen max-w-6xl mx-auto p-5 space-y-10 bg-gray-50'>
+           <h1 className='text-4xl font-semibold'>Quarterly Reports 👑</h1>
+           <div  style={{
+          background: 'linear-gradient(-33.05deg, rgb(59, 161, 223) -19.632%, rgb(10, 21, 50) 102.453%)',
+        }} className='px-10 py-20  text-center  text-white rounded-lg shadow-lg'>
+           <div className='max-w-3xl mx-auto space-y-4'>
+             <h1 className='text-8xl'>👑</h1>
+            <h2 className='text-4xl'>Premium Feature - Quarterly Reports</h2>
+      <p className='text-xl'>Access comprehensive quarterly market reports with in-depth analysis, forecasts, and investment recommendations from our expert analysts.</p>
+           </div>
+           </div>
             
         </div>
     );
