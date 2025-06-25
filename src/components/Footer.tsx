45038f068985484708e6ac8acd06975fe580d1a3
mubashirbappi <mubashirbappi@gmail.com>
2025-06-25 14:39:01 +0600

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,59 +1,70 @@
 "use client";
 import React from "react";
-import { ArrowUp, Facebook, Instagram, Twitter } from 'lucide-react';
+import {  ChevronUp, Facebook, Instagram, Twitter } from 'lucide-react';
 import logo from "../../public/logo.png"
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 const Footer = () => {
-  const handleScrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+  // const handleScrollToTop = () => {
+  //   window.scrollTo({ top: 0, behavior: "smooth" });
+  // };
 
   return (
-    <footer className="bg-gray-50 pt-10 pb-6 mt-8">
-      <div className="container mx-auto px-4 flex flex-wrap justify-between gap-8">
+    <footer className="bg-gray-50 pt-10  pb-10 mt-8">
+      <div className="max-w-7xl px-4 mx-auto flex flex-wrap justify-between gap-8">
         {/* Logo and Company Info */}
         <div className="min-w-[200px]">
-          <Image src={logo} alt="EstateAtlas Logo" width={150} height={50} className="mb-4" />
-          <p className="text-[#4D5461] font-semibold mt-2">Empower your investment <br /> decisions using our tools.</p>
+          <Image src={logo} alt="EstateAtlas Logo" width={199} height={52} className="mb-4" />
+          <p className="text-[#4D5461] text-[19px] font-semibold mt-2">Empower your investment <br /> decisions using our tools.</p>
         </div>
 
         {/* Quick Links */}
         <div className="min-w-[150px]">
-          <h4 className="text-lg font-semibold mb-2">Quick Links</h4>
-          <ul className="space-y-1">
+          <h4 className="text-2xl font-semibold mb-4">Company</h4>
+          <ul className="space-y-4">
             <li>
-              <a href="/about" className="text-gray-600 hover:text-blue-600 transition">About Us</a>
+              <a href="/about" className="text-gray-600 hover:text-blue-600 text-lg transition">About Us</a>
             </li>
             <li>
-              <a href="/properties" className="text-gray-600 hover:text-blue-600 transition">Properties</a>
+              <a href="/properties" className="text-gray-600 hover:text-blue-600 text-lg transition">Pricing</a>
             </li>
             <li>
-              <a href="/contact" className="text-gray-600 hover:text-blue-600 transition">Contact</a>
+              <a href="/contact" className="text-gray-600 hover:text-blue-600 text-lg transition">Articles</a>
+            </li>
+            <li>
+              <a href="/contact" className="text-gray-600 hover:text-blue-600 text-lg transition">Contact us</a>
             </li>
           </ul>
         </div>
 
         {/* Legal Links */}
         <div className="min-w-[150px]">
-          <h4 className="text-lg font-semibold mb-2">Legal</h4>
-          <ul className="space-y-1">
+          <h4 className="text-2xl font-semibold mb-4">Legal</h4>
+          <ul className="space-y-4">
             <li>
-              <a href="/privacy" className="text-gray-600 hover:text-blue-600 transition">Privacy Policy</a>
+              <a href="/privacy" className="text-gray-600 hover:text-blue-600 text-lg transition">Terms of service</a>
             </li>
             <li>
-              <a href="/terms" className="text-gray-600 hover:text-blue-600 transition">Terms of Service</a>
+              <a href="/terms" className="text-gray-600 hover:text-blue-600 text-lg transition">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="/terms" className="text-gray-600 hover:text-blue-600 text-lg transition">Cookies Policy</a>
+            </li>
+            <li>
+              <a href="/terms" className="text-gray-600 hover:text-blue-600 text-lg transition">Data Disclaimer</a>
             </li>
           </ul>
         </div>
 
         {/* Scroll to Top Button */}
-        <div className="min-w-[150px] flex items-start md:items-end justify-start md:justify-end">
-          <button
-            onClick={handleScrollToTop}
-            className="px-2 py-2 bg-[#0A1532] text-white rounded-full font-semibold shadow  cursor-pointer transition"
-          >
-              <ArrowUp />
-          </button>
+         <div className="min-w-[150px] relative flex items-start md:items-end justify-start md:justify-end">
+           <Button
+        size="icon"
+        className="absolute top-4 right-4 bg-gray-900 hover:bg-gray-800 p-8 text-white rounded-lg"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        <ChevronUp className="h-10 w-10" />
+      </Button>
         </div>
       </div>
 
@@ -62,7 +73,7 @@ const Footer = () => {
 
       {/*  Social Icons + Copyright */}
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4">
-        <div className=" text-sm text-center text-[#0A1532] md:text-left"> Copyrighted
+        <div className=" text-lg text-center text-[#0A1532] md:text-left"> Copyrighted
           &copy; {new Date().getFullYear()} ESTATEATLAS. All Rights Reserved
         </div>
         <div className="flex space-x-4">
