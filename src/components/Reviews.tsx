b9cd835dc3629eec4c69399754e8912572d74422
mubashirbappi <mubashirbappi@gmail.com>
2025-06-25 11:56:18 +0600

    update review section ui

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,47 +1,56 @@
 import { cn } from "@/lib/utils";
 import { Marquee } from "@/components/magicui/marquee";
+import man1 from "../../public/man1.png"; 
+import man2 from "../../public/man2.png"; 
+import man3 from "../../public/man3.png"; 
+import man4 from "../../public/man4.png"; 
+import man5 from "../../public/man5.png"; 
+import man6 from "../../public/man6.png"; 
+import Image from "next/image";
 
 const reviews = [
   {
     name: "Jack",
     username: "@jack",
     body: "EstateAtlas revolutionized how we analyze international markets. The side-by-side country comparison tool saved us 40+ hours monthly. We now identify high-yield markets twice as fast.",
-    img: "https://avatar.vercel.sh/jack",
+    img: man1,
   },
   {
     name: "Jill",
     username: "@jill",
     body: "EstateAtlas revolutionized how we analyze international markets. The side-by-side country comparison tool saved us 40+ hours monthly. We now identify high-yield markets twice as fast.",
-    img: "https://avatar.vercel.sh/jill",
+    img: man2,
   },
   {
     name: "John",
     username: "@john",
     body: "EstateAtlas revolutionized how we analyze international markets. The side-by-side country comparison tool saved us 40+ hours monthly. We now identify high-yield markets twice as fast.",
-    img: "https://avatar.vercel.sh/john",
+    img: man3,
   },
   {
     name: "Jane",
     username: "@jane",
     body: "EstateAtlas revolutionized how we analyze international markets. The side-by-side country comparison tool saved us 40+ hours monthly. We now identify high-yield markets twice as fast.",
-    img: "https://avatar.vercel.sh/jane",
+    img: man4,
   },
   {
     name: "Jenny",
     username: "@jenny",
     body: "EstateAtlas revolutionized how we analyze international markets. The side-by-side country comparison tool saved us 40+ hours monthly. We now identify high-yield markets twice as fast.",
-    img: "https://avatar.vercel.sh/jenny",
+    img: man5,
   },
   {
     name: "James",
     username: "@james",
     body: "EstateAtlas revolutionized how we analyze international markets. The side-by-side country comparison tool saved us 40+ hours monthly. We now identify high-yield markets twice as fast.",
-    img: "https://avatar.vercel.sh/james",
+    img: man6,
   },
 ];
 
 const firstRow = reviews.slice(0, reviews.length / 2);
 const secondRow = reviews.slice(reviews.length / 2);
+
+import type { StaticImageData } from "next/image";
 
 const ReviewCard = ({
   img,
@@ -49,33 +58,33 @@ const ReviewCard = ({
   username,
   body,
 }: {
-  img: string;
+  img: StaticImageData;
   name: string;
   username: string;
   body: string;
 }) => {
   return (
     <figure
-      className={cn(
-        "relative h-full w-80 cursor-pointer overflow-hidden rounded-xl border px-4 py-10 space-y-4",
-        // light styles
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-      )}
-    >
-      
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-    </figure>
+  className={cn(
+    "relative h-full w-80 cursor-pointer overflow-hidden rounded-xl px-4 py-10 space-y-4",
+    "shadow", 
+    "bg-[linear-gradient(180deg,_#ffffff,_#f3f3f3)]", 
+    "dark:bg-[linear-gradient(180deg,_#1a1a1a,_#2a2a2a)]" 
+  )}
+>
+  <blockquote className="mt-2 text-sm">{body}</blockquote>
+  <hr />
+  <div className="flex flex-row items-center gap-2">
+    <Image className="rounded-full" width="32" height="32" alt="" src={img} />
+    <div className="flex flex-col">
+      <figcaption className="text-sm font-medium dark:text-white">
+        {name}
+      </figcaption>
+      <p className="text-xs font-medium dark:text-white/40">{username}</p>
+    </div>
+  </div>
+</figure>
+
   );
 };
 
