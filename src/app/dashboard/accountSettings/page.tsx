cb2efd908dc299e96976858d8bd3376586224eeb
mubashirbappi <mubashirbappi@gmail.com>
2025-06-26 17:59:38 +0600

    update

---
+++ src/app/dashboard/accountSettings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const accountSettings = () => {
+    return (
+        <div>
+            <h1>this is account page </h1>
+            
+        </div>
+    );
+};
+
+export default accountSettings;
